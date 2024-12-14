1c40646317cfdbf2e1bebf94eb3412f39bfdad11
Shirafzx <shirafzx@gmail.com>
2024-12-14 20:00:55 +0700

    setup config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use quests_tracker::config::config_loader;
+use tracing::{error, info};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let dotenvy_env = match config_loader::load() {
+        Ok(env) => env,
+        Err(e) => {
+            error!("Failed to load ENV: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    info!("ENV has been loaded");
 }
